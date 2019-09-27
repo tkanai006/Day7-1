85b1d15dcd2ab9e64a224ee783b590d2c81846e4
takashi.kanai <t.kanai.itas006@gmail.com>
2019-09-28 02:35:47 +0900

    投稿確認画面作成

--- app/helpers/whispers_helper.rb
+++ app/helpers/whispers_helper.rb
@@ -3,7 +3,7 @@ module WhispersHelper
         if action_name == 'new' || action_name == 'confirm' || action_name == 'create'
             confirm_whispers_path
         elsif action_name == 'edit'
-           whisper_path 
+            whisper_path 
         end
     end
 end
